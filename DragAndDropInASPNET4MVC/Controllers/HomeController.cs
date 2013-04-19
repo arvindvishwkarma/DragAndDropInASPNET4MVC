15dd7587c391181a19cf91472a6ff471f0734f0b
Arvind <arvindvmail@gmail.com>
2013-04-19 10:21:33 +0530

    Project Created

---
+++ DragAndDropInASPNET4MVC/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System.IO;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DragAndDropInASPNET4MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult UploadImage(HttpPostedFileBase file)
+        {
+            UploadFile(file);
+            return Content(file.FileName);
+        }
+
+        public void UploadFile(HttpPostedFileBase file)
+        {
+            System.IO.File.WriteAllBytes(Server.MapPath("~/Content/upload/" + file.FileName), ReadData(file.InputStream));
+        }
+
+        private byte[] ReadData(Stream stream)
+        {
+            var buffer = new byte[16 * 1024];
+
+            using (var ms = new MemoryStream())
+            {
+                int read;
+                while ((read = stream.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    ms.Write(buffer, 0, read);
+                }
+
+                return ms.ToArray();
+            }
+        }
+    }
+}
---
+++ DragAndDropInASPNET4MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DragAndDropInASPNET4MVC.MvcApplication" Language="C#" %>
